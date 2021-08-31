e69c1912f28e187b649003278e95e1b4e672ad35
laikelly <lai.kellyy@gmail.com>
2021-08-30 22:01:57 -0400

    add button to homepage

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+function linkToProject() {
+    window.open("https://fmundergrad.hunter.cuny.edu/~laik/finalproject/index.html", "_blank"); 
+}
