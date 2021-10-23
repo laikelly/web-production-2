63849f67987868297822dc6b17133e8267b1ed8a
laikelly <lai.kellyy@gmail.com>
2021-10-22 23:12:17 -0400

    add search page

--- week6/script.js
+++ week6/script.js
@@ -1,21 +1,70 @@
-//onload function
+//search page js
+$(document).ready(function() {
+
+    $('.search').each(function() {
+        var self = $(this);
+        var div = self.children('div');
+        var placeholder = div.children('input').attr('placeholder');
+        var placeholderArr = placeholder.split(/ +/);
+        if (placeholderArr.length) {
+            var spans = $('<div />');
+            $.each(placeholderArr, function(index, value) {
+                spans.append($('<span />').html(value + '&nbsp;'));
+            });
+            div.append(spans);
+        }
+        self.click(function() {
+            self.addClass('open');
+            setTimeout(function() {
+                self.find('input').focus();
+            }, 750);
+        });
+
+        $(document).click(function(e) {
+            if (!$(e.target).is(self) && !jQuery.contains(self[0], e.target)) {
+                self.removeClass('open');
+            }
+        });
+        //redirects page
+        self.keypress(function(e) {
+            var code = e.keyCode ? e.keyCode : e.which;
+            if (code.toString() == 13) {
+                QLSearch(); // call the function to redirect to another page
+                return false;
+            }
+
+        });
+        // Function that redirect the user to another page
+        function QLSearch() {
+            window.location = "file:///Users/kellylai/Documents/MEDP331/web-production-2/week6/index.html";
+        }
+    });
+
+});
+
+//api js
 $(function() {
-    $("#get-input").click(function() {
-        let showTitle = $("#title").val()
-        //if it's a search URL, you could add the text input to it
+        let showTitle = $('#title').val();
+        if (showTitle != '') {
+                getShow(showTitle)
+              }
+              //reset the input
+            // $('#title').val('')
+
+
+    function getShow(showTitle){
         $.getJSON(`https://api.tvmaze.com/search/shows?q=${showTitle}`, function(data) {
             for (i = 0; i < data.length; i++) { //goes through the list of results
                 // console.log('title: ', data[i].show.name)
                 $("#show-details").append('<h1>' + data[i].show.name + '</h1>') //displays the title of the show
 
                 //displays the image of the show
-                if (data[i].show.image == null){ //checks for null values
+                if (data[i].show.image == null) { //checks for null values
                     let image = $("<img/>", {
                         src: "img/img_not_available.png"
                     })
                     image.appendTo('#show-details')
-                }
-                else{
+                } else {
                     let image = $("<img/>", {
                         src: data[i].show.image.medium
                     })
@@ -23,53 +72,36 @@ $(function() {
                 }
                 if (data[i].show.language == null) {
                     $("#show-details").append('<ul> Language: N/A </ul>')
-                }
-                else{
+                } else {
                     $("#show-details").append('<ul> Language: ' + data[i].show.language + '</ul>')
                 }
                 if (data[i].show.genres.length == 0) {
                     $("#show-details").append('<ul> Genres: N/A </ul>')
-                }
-                else {
+                } else {
                     $("#show-details").append('<ul> Genres: ' + data[i].show.genres + '</ul>')
 
                 }
                 if (data[i].show.rating.average == null) {
                     $("#show-details").append('<ul> Rating: N/A </ul>')
-                }
-                else{
+                } else {
                     $("#show-details").append('<ul> Rating: ' + data[i].show.rating.average + '</ul>')
                 }
                 if (data[i].show.ended == null) {
                     $("#show-details").append('<ul> Aired: ' + data[i].show.premiered + ' to N/A' + '</ul>')
-                }
-                else{
+                } else {
                     $("#show-details").append('<ul> Aired: ' + data[i].show.premiered + ' to ' + data[i].show.ended + '</ul>')
                 }
                 if (data[i].show.summary == null) {
                     $("#show-details").append('<p> Summary Unavailable </p>')
-                }
-                else{
+                } else {
                     $("#show-details").append('<p>' + data[i].show.summary + '</p>')
                 }
-
-                let showID = data[i].show.id
-                $.getJSON(`https://api.tvmaze.com/shows/${showID}/cast`, function(info) {
-                    console.log('cast info:', info)
-                })
             }
-
-
-
             console.log('data: ', data)
-            //add something to the HTML
-            //$("#output").text(data.something.something)
         }).fail(function() {
             //we can add a "fail" function to our AJAX request to do something if it fails
             console.log("that request failed")
         })
-        //reset the input
-        $('#text-input').val('')
 
-    })
+    }
 })
