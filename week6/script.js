f271be4f1a28bdb7b977dd06c8a2f878237f39ff
laikelly <lai.kellyy@gmail.com>
2021-10-22 23:49:59 -0400

    update redirect link

--- week6/script.js
+++ week6/script.js
@@ -28,23 +28,24 @@ $(document).ready(function() {
     });
 
     //redirects page
-    $('#title').keydown(function(e) {
+    $('#title').keypress(function(e) {
         if (e.which === 13) {
+            var userInput = $('#title').val()
+            if (userInput != ''){
+                getShow(userInput)
+            }
             window.location = "https://laikelly.github.io/web-production-2/week6/index.html";
+            $('#title').val('')
             return false;
         }
     });
 
-});
-
-//api js
-$(function() {
-    let showTitle = $('#title').val();
-    if (showTitle != '') {
-        getShow(showTitle)
-    }
-    //reset the input
-    $('#title').val('')
+    // let showTitle = userInput;
+    // if (showTitle != '') {
+    //     getShow(showTitle)
+    // }
+    // //reset the input
+    // $('#title').val('')
 
     function getShow(showTitle) {
         $.getJSON(`https://api.tvmaze.com/search/shows?q=${showTitle}`, function(data) {
@@ -98,4 +99,69 @@ $(function() {
         })
 
     }
-})
+
+
+});
+
+//api js
+// $(function() {
+//     let showTitle = $('#title').val();
+//     if (showTitle != '') {
+//         getShow(showTitle)
+//     }
+//     //reset the input
+//     $('#title').val('')
+//
+//     function getShow(showTitle) {
+//         $.getJSON(`https://api.tvmaze.com/search/shows?q=${showTitle}`, function(data) {
+//             for (i = 0; i < data.length; i++) { //goes through the list of results
+//                 // console.log('title: ', data[i].show.name)
+//                 $("#show-details").append('<h1>' + data[i].show.name + '</h1>') //displays the title of the show
+//
+//                 //displays the image of the show
+//                 if (data[i].show.image == null) { //checks for null values
+//                     let image = $("<img/>", {
+//                         src: "img/img_not_available.png"
+//                     })
+//                     image.appendTo('#show-details')
+//                 } else {
+//                     let image = $("<img/>", {
+//                         src: data[i].show.image.medium
+//                     })
+//                     image.appendTo('#show-details')
+//                 }
+//                 if (data[i].show.language == null) {
+//                     $("#show-details").append('<ul> Language: N/A </ul>')
+//                 } else {
+//                     $("#show-details").append('<ul> Language: ' + data[i].show.language + '</ul>')
+//                 }
+//                 if (data[i].show.genres.length == 0) {
+//                     $("#show-details").append('<ul> Genres: N/A </ul>')
+//                 } else {
+//                     $("#show-details").append('<ul> Genres: ' + data[i].show.genres + '</ul>')
+//
+//                 }
+//                 if (data[i].show.rating.average == null) {
+//                     $("#show-details").append('<ul> Rating: N/A </ul>')
+//                 } else {
+//                     $("#show-details").append('<ul> Rating: ' + data[i].show.rating.average + '</ul>')
+//                 }
+//                 if (data[i].show.ended == null) {
+//                     $("#show-details").append('<ul> Aired: ' + data[i].show.premiered + ' to N/A' + '</ul>')
+//                 } else {
+//                     $("#show-details").append('<ul> Aired: ' + data[i].show.premiered + ' to ' + data[i].show.ended + '</ul>')
+//                 }
+//                 if (data[i].show.summary == null) {
+//                     $("#show-details").append('<p> Summary Unavailable </p>')
+//                 } else {
+//                     $("#show-details").append('<p>' + data[i].show.summary + '</p>')
+//                 }
+//             }
+//             console.log('data: ', data)
+//         }).fail(function() {
+//             //we can add a "fail" function to our AJAX request to do something if it fails
+//             console.log("that request failed")
+//         })
+//
+//     }
+// })
